924aacdc23831383f872c25fe783ea7c62f074f7
Marc Sànchez <msanxes@gmail.com>
2020-02-09 18:45:46 +0100

    [FEAT] Curriculum ws Initialized with spring boot.

---
+++ src/main/java/com/mspifarre/curriculum_ws/CurriculumWsApplication.java
@@ -0,0 +1,13 @@
+package com.mspifarre.curriculum_ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurriculumWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurriculumWsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mspifarre/curriculum_ws/CurriculumWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mspifarre.curriculum_ws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurriculumWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
